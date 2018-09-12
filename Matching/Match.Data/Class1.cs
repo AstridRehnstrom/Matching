db3a662afbc0d1db5281b96df93dad0516571841
Ramy Niranjan <ramy.niranjan@gmail.com>
2018-09-12 16:24:46 +0200

    Creat ther DataBase

--- Matching/Match.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Match.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ Matching/Match.Data/MatchMainData.cs
@@ -0,0 +1,17 @@
+﻿using Matching.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Match.Data
+{
+    public class MatchMainData: DbContext
+    {
+        public DbSet<Person> Samurais { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+              "Server = (localdb)\\mssqllocaldb; Database = EfSamurai;");
+        }
+    }
+}
