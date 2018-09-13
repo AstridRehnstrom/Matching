e36b90e13e4bf93ae0f60b6d02f54a52646f7945
Anastasia Alekseeva <anastasia.alekseeva@gmail.com>
2018-09-13 11:23:30 +0200

    personality

--- Matching/Match.Data/Migrations/20180913092239_pipobo.Designer.cs
+++ Matching/Match.Data/Migrations/20180913092239_pipobo.Designer.cs
@@ -4,14 +4,16 @@ using Match.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Match.Data.Database.Migrations
 {
     [DbContext(typeof(MatchMainData))]
-    partial class MatchMainDataModelSnapshot : ModelSnapshot
+    [Migration("20180913092239_pipobo")]
+    partial class pipobo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
