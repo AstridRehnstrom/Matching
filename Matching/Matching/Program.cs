8a50798911157561b0d50c268b4971cb988a30cb
Astrid Rehnström <astrid.rehnstrom@gmail.com>
2018-09-14 11:43:18 +0200

    add

--- Matching/Matching/Program.cs
+++ Matching/Matching/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
 using Application;
+using Match.Data;
+using Matching.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Matching
@@ -8,9 +11,29 @@ namespace Matching
     {
         static void Main(string[] args)
         {
-            //DataInput.ClearDatabase();
-            DataInput.AddPersonWithData();
-            
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Dejting-Appen");
+            Console.ResetColor();
+          //DataInput.ClearDatabase();
+          //DataInput.AddPersonWithData();
+           ListAllPersonsWithSameInterests(PersonInterest.Literature);
+           ListAllPersonsWithSameInterests(PersonInterest.Movie);
+        }
+
+        private static void ListAllPersonsWithSameInterests(PersonInterest interest)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Show all persons with the inteserst: "+ interest);
+            Console.ResetColor();
+            using (var context = new MatchMainData())
+            {
+                foreach (var person in context.Persons
+                    .Include(q => q.InterestsPerson)
+                    .Where(p =>  p.InterestsPerson.Select(x=>x.Interests.PersonInterest).Contains(interest)))
+                {
+                    Console.WriteLine(person.FirstName + " "+ person.LastName);
+                }
+            }
         }
     }
 }
